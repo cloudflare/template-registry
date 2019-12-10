95bfa27d96a9bc5db8ef31d6cb16d62b39de801c
Kristian Freeman <kristian@kristianfreeman.com>
2019-12-10 14:04:06 -0600

    Fix HTML rewriter snippet
    Closes #10

--- templates/javascript/rewrite_links_html.js
+++ templates/javascript/rewrite_links_html.js
@@ -1,11 +1,10 @@
+const OLD_URL = 'developer.mozilla.org'
+const NEW_URL = 'mynewdomain.com'
+
 async function handleRequest(req) {
   const res = await fetch(req)
   return rewriter.transform(res)
 }
-
-const rewriter = new HTMLRewriter()
-  .on('a', new AttributeRewriter('href'))
-  .on('img', new AttributeRewriter('src'))
 
 class AttributeRewriter {
   constructor(attributeName) {
@@ -17,11 +16,15 @@ class AttributeRewriter {
     if (attribute) {
       element.setAttribute(
         this.attributeName,
-        attribute.replace('myolddomain.com', 'mynewdomain.com')
+        attribute.replace(OLD_URL, NEW_URL),
       )
     }
   }
 }
+
+const rewriter = new HTMLRewriter()
+  .on('a', new AttributeRewriter('href'))
+  .on('img', new AttributeRewriter('src'))
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
