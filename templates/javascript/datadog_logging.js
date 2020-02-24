0f9af2ec1f81e9ca9856b3395f3687a81020113b
Kristian Freeman <kristian@kristianfreeman.com>
2020-02-24 13:33:19 -0600

    add datadog logging example
    this pull request adds a snippet for showing how to log from a workers application to datadog, a logging provider. i haven't submitted a template in a while so let me know if is formatted wrong!

---
+++ templates/javascript/datadog_logging.js
@@ -0,0 +1,22 @@
+const handleRequest = evt => {
+  // Using waitUntil confirms that the log request finished before
+  // the Workers script stops executing
+  evt.waitUntil(log('Received request: ', evt.request.url))
+  return fetch(evt.request)
+}
+
+// Identifies the logging client in Datadog's UI. 
+// Can be hard-coded to something like "edge", or can be
+// customized per environment using `wrangler secret put SERVICE_NAME`
+const SERVICE_NAME = "edge"
+
+const log = body => 
+  // DATADOG_API_KEY is an API key generated in Datadog's UI
+  fetch(`https://http-intake.logs.datadoghq.com/v1/input/${DATADOG_API_KEY}`, {
+    method: 'POST',
+    body: JSON.stringify(Object.assign({}, body, { service: SERVICE_NAME })),
+    headers: { 'Content-type': 'application/json' },
+  })
+
+
+addEventListener('fetch', handleRequest)
