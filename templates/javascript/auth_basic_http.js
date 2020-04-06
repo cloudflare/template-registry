0d155e57acc6edce8b8eb42578c7ffc15df02f18
Max Ivanov <maximiv@gmail.com>
2020-04-06 18:16:34 +0100

    Add auth_basic_http.toml

--- templates/javascript/auth_basic_http.js
+++ templates/javascript/auth_basic_http.js
@@ -1,13 +1,21 @@
-/**
- * @param {string} USERNAME User name to access the page
- * @param {string} PASSWORD Password to access the page
- * @param {string} PASSWORD A name of an area (a page or a group of pages) to protect.
- * Some browsers may show "Enter user name and password to access REALM"
- */
-const USERNAME = 'demouser'
-const PASSWORD = 'demopassword'
-const REALM = 'Secure Area'
-
+async function handleRequest(request) {
+  const authorization = request.headers.get('authorization')
+  if (!request.headers.has('authorization')) {
+    return getUnauthorizedResponse(
+      'Provide User Name and Password to access this page.',
+    )
+  }
+  const credentials = parseCredentials(authorization)
+  if (credentials[0] !== USERNAME || credentials[1] !== PASSWORD) {
+    return getUnauthorizedResponse(
+      'The User Name and Password combination you have entered is invalid.',
+    )
+  }
+  return await fetch(request)
+}
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
 /**
  * Break down base64 encoded authorization string into plain-text username and password
  * @param {string} authorization
@@ -17,10 +25,8 @@ function parseCredentials(authorization) {
   const parts = authorization.split(' ')
   const plainAuth = atob(parts[1])
   const credentials = plainAuth.split(':')
-
   return credentials
 }
-
 /**
  * Helper funtion to generate Response object
  * @param {string} message
@@ -30,36 +36,15 @@ function getUnauthorizedResponse(message) {
   let response = new Response(message, {
     status: 401,
   })
-
   response.headers.set('WWW-Authenticate', `Basic realm="${REALM}"`)
-
   return response
 }
-
 /**
- * @param {Request} request
- * @returns {Response}
+ * @param {string} USERNAME User name to access the page
+ * @param {string} PASSWORD Password to access the page
+ * @param {string} REALM A name of an area (a page or a group of pages) to protect.
+ * Some browsers may show "Enter user name and password to access REALM"
  */
-async function handleRequest(request) {
-  const authorization = request.headers.get('authorization')
-
-  if (!request.headers.has('authorization')) {
-    return getUnauthorizedResponse(
-      'Provide User Name and Password to access this page.',
-    )
-  }
-
-  const credentials = parseCredentials(authorization)
-
-  if (credentials[0] !== USERNAME || credentials[1] !== PASSWORD) {
-    return getUnauthorizedResponse(
-      'The User Name and Password combination you have entered is invalid.',
-    )
-  }
-
-  return await fetch(request)
-}
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+const USERNAME = 'demouser'
+const PASSWORD = 'demopassword'
+const REALM = 'Secure Area'
