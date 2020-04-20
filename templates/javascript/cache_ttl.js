8cc9e9698e91dc6dc71e597bae91f1cd787565fb
Victoria <Bernard.victoria22@gmail.com>
2020-04-20 17:10:44 -0500

    Update cache

--- templates/javascript/cache_ttl.js
+++ templates/javascript/cache_ttl.js
@@ -5,11 +5,12 @@ async function handleRequest(request) {
   let someCustomKey = `https://${url.hostname}${url.pathname}`
   let response = await fetch(request, {
     cf: {
-      // Tell Cloudflare's Global CDN to always cache this fetch regardless of content type
+      // Tell Cloudflare's CDN to always cache this fetch regardless of content type
       // for a max of 5 seconds before revalidating the resource
-      cacheTtl: 5, // sets TTL to 5 and cacheEverything setting
+      cacheTtl: 5, // sets TTL to 5 and cacheEverything to true
       //Enterprise only feature, see Cache API for other plans
       cacheKey: someCustomKey,
+      cacheEverything: true, // override the default "cacheability" of the asset. For TTL, Cloudflare will still rely on headers set by the origin.
     },
   })
   // must use Response constructor to inherit all of response's fields
@@ -18,6 +19,6 @@ async function handleRequest(request) {
   response.headers.set('Cache-Control', 'max-age=1500')
   return response
 }
-addEventListener('fetch', event => {
+addEventListener('fetch', (event) => {
   return event.respondWith(handleRequest(event.request))
 })
