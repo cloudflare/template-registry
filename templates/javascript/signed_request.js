4b827db010e2f9f57801400ee9e883ae9884557b
Brady Joslin <brady@bradyjoslin.com>
2020-08-09 16:31:56 -0500

    Web Crypto API support in Sign and Verify Snippet

--- templates/javascript/signed_request.js
+++ templates/javascript/signed_request.js
@@ -1,35 +1,55 @@
-// NOTE Requires ESM through webpack project type
-const crypto = require('crypto')
+/**
+ * GET  ->  provides base64 signature of responseBody as HTTP Header
+ * POST ->  checks base64 signature provided as HTTP Header for the request text
+ */
 const SECRET = 'SECRET_KEY'
-async function handleRequest(request) {
-  let signed = await checkSignature(request)
-  if (signed) {
-    let responseBody = 'Hello worker!'
-    return await signResponse(responseBody, new Response(responseBody))
-  } else {
-    return new Response('Request not signed', { status: 400 })
-  }
-}
+const responseBody = 'Hello worker!'
 addEventListener('fetch', event => {
-  console.log(createHexSignature('asd'))
   event.respondWith(handleRequest(event.request))
 })
-async function createHexSignature(requestBody) {
-  let hmac = crypto.createHmac('sha256', SECRET)
-  hmac.update(requestBody)
-  return hmac.digest('hex')
+async function handleRequest(request) {
+  if (request.method === 'GET') {
+    return await signResponse(responseBody, new Response(responseBody))
+  } else if (request.method === 'POST') {
+    const isSigValid = await verifySignature(request)
+    return isSigValid
+      ? new Response('Valid signature!')
+      : new Response('Invalid signature!', { status: 400 })
+  } else {
+    return new Response('Method not supported')
+  }
 }
-async function checkSignature(request) {
-  // hash request with secret key
-  let expectedSignature = await createHexSignature(await request.text())
-  let actualSignature = await request.headers.get('signature')
-  // check that hash matches signature
-  return expectedSignature === actualSignature
+async function importKey(secret) {
+  return await crypto.subtle.importKey(
+    'raw',
+    new TextEncoder().encode(secret),
+    { name: 'HMAC', hash: 'SHA-256' },
+    false,
+    ['sign', 'verify'],
+  )
 }
 async function signResponse(responseBody, response) {
-  // create signature
-  const signature = await createHexSignature(responseBody)
-  response.headers.set('signature', signature)
-  //add header with signature
+  const key = await importKey(SECRET)
+  const signature = await crypto.subtle.sign(
+    'HMAC',
+    key,
+    new TextEncoder().encode(responseBody),
+  )
+  const sigHash = btoa(String.fromCharCode(...new Uint8Array(signature)))
+  response.headers.set('signature', sigHash)
   return response
+}
+async function verifySignature(request) {
+  const signature = request.headers.get('signature') || ''
+  const key = await importKey(SECRET)
+
+  return await crypto.subtle.verify(
+    'HMAC',
+    key,
+    base64ToUint8Array(signature),
+    new TextEncoder().encode(await request.text()),
+  )
+}
+function base64ToUint8Array(base64) {
+  return Uint8Array.from(atob(base64), c => c.charCodeAt(0))
 }
