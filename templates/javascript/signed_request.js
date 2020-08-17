6d3f6106b096b879900ea59d2c772160f98259c3
Brady Joslin <brady@bradyjoslin.com>
2020-08-17 09:26:34 -0500

    refactor signed_request.js for further clarity
    refactor signed_request.js for clarity

--- templates/javascript/signed_request.js
+++ templates/javascript/signed_request.js
@@ -1,17 +1,27 @@
 /**
- * GET  ->  provides base64 signature of responseBody as HTTP Header
- * POST ->  checks base64 signature provided as HTTP Header for the request text
+ * GET -> GET -> reply with an HTTP response header set to the signature for the text provided as a query string
+ * POST -> check the validity of base64 signature provided as an HTTP header for message in the HTTP body
  */
 const SECRET = 'SECRET_KEY'
-const responseBody = 'Hello worker!'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 async function handleRequest(request) {
   if (request.method === 'GET') {
-    return await signResponse(responseBody, new Response(responseBody))
+    const url = new URL(request.url)
+    const msg = url.searchParams.get('msg') || ''
+    const signature = await signResponse(msg, SECRET)
+    let response = new Response(msg)
+    response.headers.set('signature', signature)
+
+    return response
   } else if (request.method === 'POST') {
-    const isSigValid = await verifySignature(request)
+    const message = await request.text()
+    const signature = request.headers.get('signature') || ''
+    const isSigValid = await verifySignature(message, signature, SECRET)
+
     return isSigValid
       ? new Response('Valid signature!')
       : new Response('Invalid signature!', { status: 400 })
@@ -19,6 +29,7 @@ async function handleRequest(request) {
     return new Response('Method not supported')
   }
 }
+
 async function importKey(secret) {
   return await crypto.subtle.importKey(
     'raw',
@@ -28,27 +39,29 @@ async function importKey(secret) {
     ['sign', 'verify'],
   )
 }
-async function signResponse(responseBody, response) {
-  const key = await importKey(SECRET)
+
+async function signResponse(message, secret) {
+  const key = await importKey(secret)
   const signature = await crypto.subtle.sign(
     'HMAC',
     key,
-    new TextEncoder().encode(responseBody),
+    new TextEncoder().encode(message),
   )
-  // Convert Uint8Array to Base64
-  const sigHash = btoa(String.fromCharCode(...new Uint8Array(signature)))
-  response.headers.set('signature', sigHash)
-  return response
+
+  // Convert ArrayBuffer to Base64
+  return btoa(String.fromCharCode(...new Uint8Array(signature)))
 }
-async function verifySignature(request) {
-  const signature = request.headers.get('signature') || ''
-  const key = await importKey(SECRET)
+
+async function verifySignature(message, signature, secret) {
+  const key = await importKey(secret)
+
   // Convert Base64 to Uint8Array
   const sigBuf = Uint8Array.from(atob(signature), c => c.charCodeAt(0))
+
   return await crypto.subtle.verify(
     'HMAC',
     key,
     sigBuf,
-    new TextEncoder().encode(await request.text()),
+    new TextEncoder().encode(message),
   )
 }
